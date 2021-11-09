b68a835d0630f13861d73ce9c7aa2f9e1392a673
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-09 17:48:35 -0500

    intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,7 +4,15 @@ import "./Intro.scss";
 function Intro() {
     return (
         <div className="intro" id="intro">
-            <div className="left"></div>
+            <div className="left">
+                <div className="imgContainer">
+                    <img
+                        src="https://i.ibb.co/JvNVVqY/portfinal.png"
+                        alt="portfinal"
+                        border="0"
+                    ></img>
+                </div>
+            </div>
             <div className="right"></div>
         </div>
     );
