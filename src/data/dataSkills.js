8cb7a0233010cb13fadd6cf986143ea4d7611a4f
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-16 21:50:16 -0500

    commit skills

--- src/data/dataSkills.js
+++ src/data/dataSkills.js
@@ -2,7 +2,8 @@ import { IoLogoHtml5 as Html } from "react-icons/io";
 import { IoLogoCss3 as Css } from "react-icons/io";
 import { IoLogoJavascript as Js } from "react-icons/io";
 import { IoLogoSass as Sass } from "react-icons/io";
-import { IoLogoReact as React } from "react-icons/io";
+import { DiReact as React } from "react-icons/di";
+import { SiBootstrap as Bootstrap } from "react-icons/si";
 
 export const skills = [
     {
@@ -26,8 +27,13 @@ export const skills = [
         tech: "Frontend",
     },
     {
-        icon: <Html />,
-        txt: "Html",
+        icon: <React />,
+        txt: "React",
+        tech: "Frontend",
+    },
+    {
+        icon: <Bootstrap />,
+        txt: "Bootstrap",
         tech: "Frontend",
     },
 ];
