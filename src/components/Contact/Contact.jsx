8ea97c754386f50ffd03e01874a0076b7dcd39d6
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-20 02:24:54 -0500

    form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,44 @@ import "./Contact.scss";
 function Contact() {
     return (
         <div className="contact" id="contact">
-            contact
+            <div className="maxCont">
+                <div className="txt">
+                    <h1>CONTACT ME</h1>
+                </div>
+                <form action="#">
+                    <div className="formRow">
+                        <div className="inputData">
+                            <input type="text" />
+                            <label>First Name</label>
+                        </div>
+                        <div className="inputData">
+                            <input type="text" />
+                            <label>Last Name</label>
+                        </div>
+                    </div>
+                    <div className="formRow">
+                        <div className="inputData">
+                            <input type="text" />
+                            <label>Email Address</label>
+                        </div>
+                        <div className="inputData">
+                            <input type="text" />
+                            <label>Contact number</label>
+                        </div>
+                    </div>
+                    <div className="formRow textarea">
+                        <div className="inputData">
+                            <textarea cols="30" rows="10"></textarea>
+                            <label>Email Address</label>
+                        </div>
+                    </div>
+                    <div className="formRow textarea">
+                        <div className="inputData">
+                            <button>submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/SocialNet/SocialNet.jsx
+++ src/components/SocialNet/SocialNet.jsx
@@ -3,42 +3,55 @@ import "./SocialNet.scss";
 import { AiOutlineLinkedin as Linkedin } from "react-icons/ai";
 import { AiOutlineGithub as Github } from "react-icons/ai";
 import { AiOutlineGitlab as Gitlab } from "react-icons/ai";
+import { SiVercel as Vercel } from "react-icons/si";
 
 function Footer() {
     return (
-        <div className="wrap">
-            <div className="personal">
-                <h3>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Inventore quis magnam quod culpa deleniti, ducimus ut,
-                    minima dolores, soluta fugiat libero reiciendis? Beatae in
-                    asperiores blanditiis maxime quaerat tempora illum.
-                </h3>
+        <div className="containerWrap" id="socialNet">
+            <div className="title">
+                <h1>SOCIAL NETWORKS</h1>
             </div>
-            <div className="socialNet" id="socialNet">
-                <a
-                    className="link"
-                    href="https://www.linkedin.com/in/hiromi-acosta-quevedo-2831381ba/"
-                    target="__blank"
-                >
-                    <Linkedin />
-                </a>
-                <a
-                    className="link"
-                    href="https://github.com/maquevedo7"
-                    target="__blank"
-                >
-                    <Github />
-                </a>
-                <a
-                    className="link"
-                    href="https://gitlab.com/maquevedo7"
-                    target="__blank"
-                >
-                    <Gitlab />
-                </a>
+            <div className="wrap">
+                <div className="personal">
+                    <p>
+                        I am a cheerful, communicative, strong and risk-taking
+                        person. I really like to learn new things and improve
+                        every day. I am a fast learner, and I like to perfect
+                        that knowledge as best I can to apply it in other
+                        contexts. I am adaptable and I like to know the world.
+                    </p>
+                </div>
+                <div className="socialNet">
+                    <a
+                        className="link"
+                        href="https://www.linkedin.com/in/hiromi-acosta-quevedo-2831381ba/"
+                        target="__blank"
+                    >
+                        <Linkedin />
+                    </a>
+                    <a
+                        className="link"
+                        href="https://github.com/maquevedo7"
+                        target="__blank"
+                    >
+                        <Github />
+                    </a>
+                    <a
+                        className="link"
+                        href="https://gitlab.com/maquevedo7"
+                        target="__blank"
+                    >
+                        <Gitlab />
+                    </a>
+                    <a
+                        className="link"
+                        href="https://vercel.com/maquevedo7"
+                        target="__blank"
+                    >
+                        <Vercel />
+                    </a>
+                </div>
             </div>
-            .
         </div>
     );
 }
