540154444f8e068a8bdec8c7146971f5a37a70bd
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-30 18:05:00 -0500

    spicy

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,44 +1,95 @@
-import React from "react";
+import React, { useRef } from "react";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import "./Contact.scss";
+import emailjs from "emailjs-com";
 
 function Contact() {
+    const form = useRef();
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "gmailMessage",
+                "template_zvut4k8",
+                form.current,
+                "user_p76k3ixuleJzrb8MZ3wSP"
+            )
+            .then(
+                (result) => {
+                    alert("Your message is sent successfully!");
+                },
+                (error) => {
+                    alert(error.message);
+                }
+            );
+        e.target.reset();
+    }
+
     return (
-        <div className="contact" id="contact">
-            <div className="maxCont">
-                <div className="txt">
-                    <h1>CONTACT ME</h1>
+        <div className="containerContactMe">
+            <div className="contactMe">
+                <h1>contact</h1>
+            </div>
+            <div className="pContac">
+                <p>Thanks for your interest in getting in touch!</p>
+                <br />
+                <p>
+                    If you have a question about my portfolio or my volunteering
+                    experience, or if you’d just like to get in touch please
+                    reach out!
+                </p>
+            </div>
+            <div className="containerContacInfo">
+                <div className="info">
+                    <h2 className="infoGeneral">Gmail</h2>
+                    <h2 className="infoGeneral">mtoro0926@gmail.com</h2>
                 </div>
-                <form action="#">
-                    <div className="formRow">
-                        <div className="inputData">
-                            <input type="text" />
-                            <label>First Name</label>
-                        </div>
-                        <div className="inputData">
-                            <input type="text" />
-                            <label>Last Name</label>
-                        </div>
+                <div className="info">
+                    <h2 className="infoGeneral">Phone</h2>
+                    <h2 className="infoGeneral">+57 324-5627089</h2>
+                </div>
+            </div>
+
+            <div className="formContainer">
+                <form ref={form} onSubmit={sendEmail}>
+                    <h2 className="title">Send me an email</h2>
+                    <div>
+                        <label className="labels">Name</label>
+                        <input
+                            className="inputs"
+                            type="text"
+                            name="to_name"
+                            placeholder="Name"
+                        ></input>
                     </div>
-                    <div className="formRow">
-                        <div className="inputData">
-                            <input type="text" />
-                            <label>Email Address</label>
-                        </div>
-                        <div className="inputData">
-                            <input type="text" />
-                            <label>Contact number</label>
-                        </div>
+                    <div>
+                        <label className="labels">Email</label>
+                        <input
+                            className="inputs"
+                            type="text"
+                            name="from_name"
+                            placeholder="Email"
+                        ></input>
                     </div>
-                    <div className="formRow textarea">
-                        <div className="inputData">
-                            <textarea cols="30" rows="10"></textarea>
-                            <label>Email Address</label>
-                        </div>
+                    <div>
+                        <label className="labels">Message</label>
+                        <input
+                            className="inputs"
+                            type="text"
+                            name="message"
+                            placeholder="Message"
+                        ></input>
                     </div>
-                    <div className="formRow textarea">
-                        <div className="inputData">
-                            <button>submit</button>
-                        </div>
+                    <div>
+                        <input
+                            className="submit"
+                            type="submit"
+                            value="Send message"
+                        ></input>
                     </div>
                 </form>
             </div>
--- src/data/dataProject.js
+++ src/data/dataProject.js
@@ -1,4 +1,7 @@
 import { IoLogoJavascript as Js } from "react-icons/io";
+import { IoLogoHtml5 as Html } from "react-icons/io";
+import { IoLogoCss3 as Css } from "react-icons/io";
+import { IoLogoReact as React } from "react-icons/io";
 
 export const projects = [
     {
@@ -7,5 +10,19 @@ export const projects = [
         description: "Retos enfocados al desarrollo de Js",
         github: "https://github.com/maquevedo7/landing_challenges",
         deploy: "https://landing-challenges-hazel.vercel.app/",
+    },
+    {
+        name: "Tribute Page",
+        icon: [<Html />, <Css />],
+        description: "Retos enfocados al desarrollo de Js",
+        github: "https://github.com/maquevedo7/landing_challenges",
+        deploy: "https://vercel.com/maquevedo7/kimetsu-no-yaiba",
+    },
+    {
+        name: "Spicy Burger",
+        icon: [<React />, <Css />],
+        description: "Primer cliente real, plataforma para compras",
+        github: "https://github.com/maquevedo7/spicy-burger",
+        deploy: "https://spicy-burger.vercel.app/",
     },
 ];
